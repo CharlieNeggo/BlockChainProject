022178d8cdb9b5f8634bfd595104ae13cc39671c
Charlie Ngo <89501197+CharlieNeggo@users.noreply.github.com>
2022-02-05 13:22:53 -0500

    Initial commit 0.1v

---
+++ blockchain.py
@@ -0,0 +1,27 @@
+# Block Chain
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import dataclasses
+import hashlib
+import json
+from flask import Flask, jsonify
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('Block Chain')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+class BlockChain:
+
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+
+    def create_block(self, proof, ):
